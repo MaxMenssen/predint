3643a281164f27f10487845465e6eb0a7efed80a
Max Menssen <menssen@biostat.uni-hannover.de>
2021-05-03 14:48:03 +0200

    fixed typos changed examples

--- R/data_documentation.R
+++ R/data_documentation.R
@@ -5,34 +5,21 @@
 #' Beta-binomial data (example 1)
 #'
 #' This data set contains sampled beta-binomial data from 10 clusters
-#' each of size 50.
+#' each of size 50. The data set was sampled with \code{rbbinom(n=10, size=50, prob=0.1, rho=0.06)}.
 #'
-#' @format A data frame with 10 rows and 2 colums:
+#' @format A \code{data.frame} with 10 rows and 2 colums:
 #' \describe{
 #'   \item{succ}{number of successes}
 #'   \item{fail}{number of failures}
 #' }
 #'
 #' @examples
-#' # Sampling of bb_dat1
-#' set.seed(234)
-#' bb_dat1 <- rbbinom(n=10, size=50, prob=0.1, rho=0.06)
-#' bb_dat1
-#'
-#' # Sampling of bb_dat2
-#' set.seed(234)
-#' bb_dat2 <- rbbinom(n=3, size=c(40, 50, 60), prob=0.1, rho=0.06)
-#' bb_dat2
-#'
 #' # Prediction interval using bb_dat2 as future data
-#' beta_bin_pi(histdat=bb_dat1, newdat=bb_dat2, nboot=100)
+#' \donttest{beta_bin_pi(histdat=bb_dat1, newdat=bb_dat2, nboot=100)}
 #'
-#' # Upper prediction bound for m=3 future number of successes
+#' # Upper prediction limit for m=3 future number of successes
 #' # that are based on cluster sizes 40, 50, 60 respectively
-#' beta_bin_pi(histdat=bb_dat1, newsize=c(40, 50, 60), alternative="upper", nboot=100)
-#'
-#' # Please note, that nboot is set to 100 in order to increase computing time. For a
-#' # valid analysis, set nboot=10000.
+#' \donttest{beta_bin_pi(histdat=bb_dat1, newsize=c(40, 50, 60), alternative="upper", nboot=100)}
 #'
 "bb_dat1"
 
@@ -42,9 +29,9 @@
 #' Beta-binomial data (example 2)
 #'
 #' This data set contains sampled beta-binomial data from 3 clusters with
-#' different size, each.
+#' different size, each. The data set was sampled with \code{rbbinom(n=3, size=c(40, 50, 60), prob=0.1, rho=0.06)}.
 #'
-#' @format A data frame with 3 rows and 2 columns:
+#' @format A \code{data.frame} with 3 rows and 2 columns:
 #' \describe{
 #'   \item{succ}{number of successes}
 #'   \item{fail}{number of failures}
@@ -52,25 +39,8 @@
 #'
 #'
 #' @examples
-#' # Sampling of bb_dat1
-#' set.seed(234)
-#' bb_dat1 <- rbbinom(n=10, size=50, prob=0.1, rho=0.06)
-#' bb_dat1
-#'
-#' # Sampling of bb_dat2
-#' set.seed(234)
-#' bb_dat2 <- rbbinom(n=3, size=c(40, 50, 60), prob=0.1, rho=0.06)
-#' bb_dat2
-#'
 #' # Prediction interval using bb_dat2 as future data
-#' beta_bin_pi(histdat=bb_dat1, newdat=bb_dat2, nboot=100)
-#'
-#' # Upper prediction bound for m=3 future number of successes
-#' # that are based on cluster sizes 40, 50, 60 respectively
-#' beta_bin_pi(histdat=bb_dat1, newsize=c(40, 50, 60), alternative="upper", nboot=100)
-#'
-#' # Please note, that nboot is set to 100 in order to increase computing time. For a
-#' # valid analysis, set nboot=10000.
+#' \donttest{beta_bin_pi(histdat=bb_dat1, newdat=bb_dat2)}
 #'
 "bb_dat2"
 
@@ -81,9 +51,9 @@
 #' Quasi-binomial data (example 1)
 #'
 #' This data set contains sampled quasi-binomial data from from 10 clusters
-#' each of size 50.
+#' each of size 50. The data set was sampled with \code{rqbinom(n=10, size=50, prob=0.1, phi=3)}.
 #'
-#' @format A data frame with 3 rows and 2 columns:
+#' @format A \code{data.frame} with 3 rows and 2 columns:
 #' \describe{
 #'   \item{succ}{number of succes}
 #'   \item{fail}{number of failures}
@@ -91,25 +61,12 @@
 #'
 #'
 #' @examples
-#' # Sampling of qb_dat1
-#' set.seed(456)
-#' qb_dat1 <- rqbinom(n=10, size=50, prob=0.1, phi=3)
-#' qb_dat1
-#'
-#' # Sampling of qb_dat2
-#' set.seed(456)
-#' qb_dat2 <- rqbinom(n=3, size=c(40, 50, 60), prob=0.1, phi=3)
-#' qb_dat2
-#'
 #' # Prediction interval using qb_dat2 as future data
-#' quasi_bin_pi(histdat=qb_dat1, newdat=qb_dat2, nboot=100)
+#' \donttest{quasi_bin_pi(histdat=qb_dat1, newdat=qb_dat2)}
 #'
-#' # Upper prediction bound for m=3 future observations
+#' # Upper prediction limit for m=3 future observations
 #' # that are based on cluster sizes 40, 50, 60 respectively
-#' quasi_bin_pi(histdat=qb_dat1, newsize=c(40, 50, 60), alternative="upper", nboot=100)
-#'
-#' # Please note, that nboot is set to 100 in order to increase computing time. For a
-#' # valid analysis, set nboot=10000.
+#' \donttest{quasi_bin_pi(histdat=qb_dat1, newsize=c(40, 50, 60), alternative="upper")}
 #'
 "qb_dat1"
 
@@ -119,9 +76,9 @@
 #' Quasi-binomial data (example 2)
 #'
 #' This data set contains sampled quasi binomial data from 3 clusters with
-#' different size.
+#' different size.The data set was sampled with \code{rqbinom(n=3, size=c(40, 50, 60), prob=0.1, phi=3)}.
 #'
-#' @format A data frame with 3 rows and 2 columns:
+#' @format A \code{data.frame} with 3 rows and 2 columns:
 #' \describe{
 #'   \item{succ}{number of succes}
 #'   \item{fail}{number of failures}
@@ -129,25 +86,8 @@
 #'
 #'
 #' @examples
-#' # Sampling of qb_dat1
-#' set.seed(456)
-#' qb_dat1 <- rqbinom(n=10, size=50, prob=0.1, phi=3)
-#' qb_dat1
-#'
-#' # Sampling of qb_dat2
-#' set.seed(456)
-#' qb_dat2 <- rqbinom(n=3, size=c(40, 50, 60), prob=0.1, phi=3)
-#' qb_dat2
-#'
 #' # Prediction interval using qb_dat2 as future data
-#' quasi_bin_pi(histdat=qb_dat1, newdat=qb_dat2, nboot=100)
-#'
-#' # Upper prediction bound for m=3 future observations
-#' # that are based on cluster sizes 40, 50, 60 respectively
-#' quasi_bin_pi(histdat=qb_dat1, newsize=c(40, 50, 60), alternative="upper", nboot=100)
-#'
-#' # Please note, that nboot is set to 100 in order to increase computing time. For a
-#' # valid analysis, set nboot=10000.
+#' \donttest{quasi_bin_pi(histdat=qb_dat1, newdat=qb_dat2)}
 #'
 "qb_dat2"
 
@@ -158,30 +98,16 @@
 #' Quasi-poisson data (example 1)
 #'
 #' This data set contains sampled quasi-poisson data for 10 clusters.
+#' The data set was sampled with \code{rqpois(n=10, lambda=50, phi=3)}.
 #'
-#' @format An integer vector with ten entries containing quasi poisson data:
+#' @format An integer vector with ten entries containing quasi poisson data
 #'
 #' @examples
-#' set.seed(123)
-#' qp_dat1 <- rqpois(n=10, lambda=50, phi=3)
-#' qp_dat1
+#' # Prediction interval using bb_dat2 as future data
+#' \donttest{quasi_pois_pi(histdat=data.frame(qp_dat1), newdat=data.frame(qp_dat2))}
 #'
-#' set.seed(123)
-#' qp_dat2 <- rqpois(n=3, lambda=50, phi=3)
-#' qp_dat2
-#'
-#'#' # Prediction interval using bb_dat2 as future data
-#' quasi_pois_pi(histdat=data.frame(qp_dat1),
-#'               newdat=data.frame(qp_dat2),
-#'               nboot=100)
-#'
-#' # Upper prediction bound for m=3 future observations
-#' quasi_pois_pi(histdat=data.frame(qp_dat1),
-#'               m=3,
-#'               alternative="upper", nboot=100)
-#'
-#' # Please note, that nboot is set to 100 in order to increase computing time. For a
-#' # valid analysis, set nboot=10000.
+#' # Upper prediction limit for m=3 future observations
+#' \donttest{quasi_pois_pi(histdat=data.frame(qp_dat1), m=3, alternative="upper")}
 #'
 "qp_dat1"
 
@@ -191,25 +117,13 @@
 #' Quasi-poisson data (example 2)
 #'
 #' This data set contains sampled quasi-poisson data for 3 clusters.
+#' The data set was sampled with \code{rqpois(n=3, lambda=50, phi=3)}.
 #'
-#' @format An integer vector with three entries containing quasi poisson data:
+#' @format An integer vector with three entries containing quasi poisson data
 #'
 #' @examples
-#' set.seed(123)
-#' qp_dat1 <- rqpois(n=10, lambda=50, phi=3)
-#' qp_dat1
-#'
-#' set.seed(123)
-#' qp_dat2 <- rqpois(n=3, lambda=50, phi=3)
-#' qp_dat2
-#'
 #' # Prediction interval using bb_dat2 as future data
-#' quasi_pois_pi(histdat=data.frame(qp_dat1),
-#'               newdat=data.frame(qp_dat2),
-#'               nboot=100)
-#'
-#' # Please note, that nboot is set to 100 in order to increase computing time. For a
-#' # valid analysis, set nboot=10000.
+#' \donttest{quasi_pois_pi(histdat=data.frame(qp_dat1), newdat=data.frame(qp_dat2))}
 #'
 "qp_dat2"
 
@@ -222,7 +136,7 @@
 #'
 #' c2_dat1 contains data that is sampled from a balanced cross-classified design.
 #'
-#' @format A data frame with 27 rows and 3 columns:
+#' @format A \code{data.frame} with 27 rows and 3 columns:
 #' \describe{
 #'   \item{y_ijk}{observations}
 #'   \item{a}{treatment a}
@@ -240,13 +154,10 @@
 #' summary(fit)
 #'
 #' # Prediction interval using c2_dat2 as future data
-#' \donttest{lmer_pi(model=fit, newdat=c2_dat2, alternative="both", nboot=100)}
+#' \donttest{lmer_pi(model=fit, newdat=c2_dat2, alternative="both")}
 #'
-#' # Upper prediction bound for m=3 future observations
-#' \donttest{lmer_pi(model=fit, m=3, alternative="upper", nboot=100)}
-#'
-#' # Please note, that nboot is set to 100 in order to increase computing time. For a
-#' # valid analysis, set nboot=10000.
+#' # Upper prediction limit for m=3 future observations
+#' \donttest{lmer_pi(model=fit, m=3, alternative="upper")}
 #'
 "c2_dat1"
 
@@ -257,7 +168,7 @@
 #'
 #' c2_dat2 contains data that was sampled from an unbalanced cross-classified design.
 #'
-#' @format A data frame with 21 rows and 3 columns:
+#' @format A \code{data.frame} with 21 rows and 3 columns:
 #' \describe{
 #'   \item{y_ijk}{observations}
 #'   \item{a}{treatment a}
@@ -275,10 +186,7 @@
 #' summary(fit)
 #'
 #' # Prediction interval using c2_dat2 as future data
-#' \donttest{lmer_pi(model=fit, newdat=c2_dat2, alternative="both", nboot=100)}
-#'
-#' # Please note, that nboot is set to 100 in order to increase computing time. For a
-#' # valid analysis, set nboot=10000.
+#' \donttest{lmer_pi(model=fit, newdat=c2_dat2, alternative="both")}
 #'
 "c2_dat2"
 
