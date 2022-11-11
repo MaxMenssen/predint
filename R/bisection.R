98608f4c5880acea445de6f5a0432953c9e8cfa0
MaxMenssen <menssen@biostat.uni-hannover.de>
2022-11-11 14:13:59 +0100

    documentation and checks of input objects

--- R/bisection.R
+++ R/bisection.R
@@ -2,6 +2,45 @@
 
 
 
+#' Bisection algorithm for bootstrap calibration of prediction intervals
+#'
+#' This helper function returns a bootstrap calibrated coefficient for the calculation
+#' of prediction intervals (and limits).
+#'
+#' @param y_star_hat a list of length \eqn{B} that contains the expected future observations.
+#' Each entry in this list has to be a numeric vector of length \eqn{M}.
+#' @param pred_se a list of length \eqn{B} that contains the standard errors of the prediction.
+#' Each entry in this list has to be a numeric vector of length \eqn{M}.
+#' @param y_star a list of length \eqn{B} that contains the future observations.
+#' Each entry in this list has to be a numeric vector of length \eqn{M}.
+#' @param alternative either "both", "upper" or "lower".
+#' \code{alternative} specifies if a prediction interval or
+#' an upper or a lower prediction limit should be computed
+#' @param quant_min lower start value for bisection
+#' @param quant_max upper start value for bisection
+#' @param n_bisec maximal number of bisection steps
+#' @param tol tolerance for the coverage probability in the bisection
+#' @param alpha defines the level of confidence (\eqn{1-\alpha})
+#' @param traceplot if \code{TRUE}: Plot for visualization of the bisection process
+#'
+#' @details This function is an implementation of the bisection algorithm of Menssen
+#' and Schaarschmidt 2021. It returns a calibrated coefficient \eqn{q^{calib}} for the
+#' calculation of simultanious prediction intervals (or limits)
+#' \deqn{[l,u] = \hat{y}^*_m  \pm q^{calib} se(Y_m - \hat{y}^*_m)}
+#' that cover all of \eqn{m=1, ... , M} future observations. \cr
+#' In this notation, \eqn{\hat{y}^*_m} are the expected future observation, \eqn{q^{calib}} are the
+#' calibrated coefficient and \eqn{se(Y_m - \hat{y}^*_m)}
+#' are the standard errors of the prediction. \cr
+#'
+#' @return This function returns \eqn{q^{calib}} in the equation above.
+#'
+#' @references Menssen and Schaarschmidt (2021): Prediction intervals for all of M future
+#' observations based on linear random effects models. Statistica Neerlandica,
+#'  \doi{10.1111/stan.12260}
+#'
+#' @export
+#'
+#'
 bisection <- function(y_star_hat,
                       pred_se,
                       y_star,
@@ -11,9 +50,93 @@ bisection <- function(y_star_hat,
                       n_bisec,
                       tol,
                       alpha,
-                      traceplot=true){
+                      traceplot=TRUE){
 
-        # Needs several checks!!!!
+        # y_star_hat has to be a list
+        if(!is.list(y_star_hat)){
+                stop("!is.list(y_star_hat)")
+        }
+
+        # pred_se has to be a list
+        if(!is.list(pred_se)){
+                stop("!is.list(pred_se)")
+        }
+
+        # y_star has to be a list
+        if(!is.list(y_star)){
+                stop("!is.list(y_star)")
+        }
+
+        # all three lists have to have the same length
+        if(length(unique(c(length(y_star_hat), length(pred_se), length(y_star)))) != 1){
+                stop("length(unique(c(length(y_star_hat), length(pred_se), length(y_star)))) != 1")
+        }
+
+        # all elements of y_star_hat have to be of length M
+        if(length(unique(sapply(y_star_hat, length))) != 1){
+                stop("all elements in y_star_hat have to have the same length M")
+        }
+
+        # all elements of pred_se have to be of length M
+        if(length(unique(sapply(pred_se, length))) != 1){
+                stop("all elements in pred_se have to have the same length M")
+        }
+
+        # all elements of y_star have to be of length M
+        if(length(unique(sapply(y_star, length))) !=1){
+                stop("all elements in y_star have to have the same length M")
+        }
+
+        # M has to be the same for all three lists
+        if(length(unique(unique(sapply(y_star_hat, length)), unique(sapply(pred_se, length)), unique(sapply(y_star, length)))) != 1){
+                stop("M differs between y_star_hat, pred_se and y_star")
+        }
+
+
+        # alternative must be defined
+        if(isTRUE(alternative!="both" && alternative!="lower" && alternative!="upper")){
+                stop("alternative must be either both, lower or upper")
+        }
+
+        # quant_min needs to be numeric
+        if(!is.numeric(quant_min)){
+                stop("!is.numeric(quant_min)")
+        }
+
+        # quant_max needs to be numeric
+        if(!is.numeric(quant_max)){
+                stop("!is.numeric(quant_max)")
+        }
+
+        # n_bisec needs ot be an integer number
+        if(!is.numeric(n_bisec)){
+                stop("!is.numeric(n_bisec)")
+        }
+
+        if(!all(n_bisec == floor(n_bisec))){
+                stop("!all(n_bisec == floor(n_bisec))")
+        }
+
+        # tolerance needs to bve a number
+        if(!is.numeric(tol)){
+                stop("!is.numeric(tol)")
+        }
+
+        # Tolerance needs to be bigger than 0
+        if(!(tol > 0)){
+                stop("tol needs to be bigger than 0")
+        }
+
+        # Tolerance needs to be small to yield accurate results
+        if(tol>0.01){
+                warning("The tolerance is higher than 0.01: The bisection resulds might be imprecise.")
+        }
+
+        if(!isTRUE(traceplot)){
+                if(!(traceplot==FALSE)){
+                        stop("traceplote needs to be TRUE or FALSE")
+                }
+        }
 
         #----------------------------------------------------------------------
 
