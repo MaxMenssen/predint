ad7e34e5d18167e3674c77110bb65d26bd89a711
MaxMenssen <menssen@biostat.uni-hannover.de>
2023-01-19 10:45:53 +0100

    get predictions as data.frame

---
+++ R/as.data.frame.predint.R
@@ -0,0 +1,39 @@
+
+
+
+
+#' Store prediction intervals or limits as a \code{data.frame}
+#'
+#' Get the prediction intervals or limits of an object of class \code{predint} and
+#' save them as a \code{data.frame}.
+#'
+#' @param x object of class \code{predint}
+#'
+#' @return This function returns the prediction intervals or limits stored in an
+#' object of class \code("predint") as a \code{data.frame}
+#'
+#' @export
+#'
+#' @examples
+#' ### PI for quasi-Poisson data
+#' pred_int <- quasi_pois_pi(histdat=data.frame(qp_dat1),
+#'                           newsize=c(1,2,3),
+#'                           nboot=100,
+#'                           traceplot = FALSE)
+#'
+#' # Return the prediction intervals as a data.frame
+#' as.data.frame(pred_int)
+as.data.frame.predint <- function(x){
+
+        # input needs to be a predint object
+        if(!inherits(x, "predint")){
+                stop("x must be of class predint")
+        }
+
+        return(pred_int$prediction)
+}
+
+
+
+
+
